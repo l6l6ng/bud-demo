2721f69de296ffd199088d5ee7eaf15bf2b0f680
liulong@psylife.cc <dfsd534@qq.com>
2023-03-03 23:57:28 +0800

    1

---
+++ controller/controller.go
@@ -0,0 +1,23 @@
+package controller
+
+import (
+	context "context"
+
+	"github.com/matthewmueller/hackernews"
+)
+
+type Controller struct {
+	HN *hackernews.Client
+}
+
+// Index of stories
+// GET /
+func (c *Controller) Index(ctx context.Context) (stories []*hackernews.Story, err error) {
+	return c.HN.FrontPage(ctx)
+}
+
+// Show story
+// GET /:id
+func (c *Controller) Show(ctx context.Context, id int) (story *hackernews.Story, err error) {
+	return c.HN.Find(ctx, id)
+}
